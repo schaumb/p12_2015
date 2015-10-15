96ecaa6cdcdd1965d59eb78be16d3ce58a2d88e2
schaumb <schaumb@gmail.com>
2015-10-15 18:21:30 +0200

    div

--- 4_Nagy_szamok/4/bn/main.cpp
+++ 4_Nagy_szamok/4/bn/main.cpp
@@ -2,6 +2,8 @@
 #include <cassert>
 #include <vector>
 #include <sstream>
+#include <fstream>
+#include <tuple>
 
 #ifndef WIN32
 void gotoxy(int x, int y)
@@ -22,10 +24,11 @@ void gotoxy(int x, int y)
 #endif // WIN32
 
 static std::string clr(100, ' ');
+std::ofstream debug("asd.txt");
 
 class BigNum {
 public:
-    BigNum(std::ostream& o = std::cout) : outp(&o) {}
+    BigNum(std::ostream& o = std::cout, const std::string& str = "") : number(str), outp(&o) {}
 
     BigNum& operator=(const std::string& str) {
         auto it = str.begin();
@@ -47,9 +50,13 @@ public:
         }
         return *this;
     }
+    friend BigNum abs(BigNum&& bn);
+    friend BigNum operator-(const BigNum& lhs, const BigNum& rhs);
+    friend BigNum operator*(const BigNum& lhs, const BigNum& rhs);
+    friend std::pair<BigNum, BigNum> operator/(const BigNum& lhs, const BigNum& rhs);
+    friend bool operator<(const BigNum& lhs, const BigNum& rhs);
 
-    BigNum sqrt() {
-        // TODO
+    BigNum sqrt(std::ostream& out = std::cout) {
         return *this;
     }
 
@@ -66,12 +73,33 @@ public:
         return *this;
     }
 
+    BigNum& operator++() {
+        auto it = number.rbegin();
+        while(it != number.rend()) {
+            if(*it < '9') {
+                ++*it;
+                return *this;
+            }
+            *it = '0';
+            ++it;
+        }
+        *this += '1';
+        return *this;
+    }
+
     std::string number;
     std::ostream* outp;
 };
 
+BigNum abs(BigNum&& bn) {
+    if(bn.number[0] == '-') {
+        bn.number = bn.number.substr(1);
+    }
+    return bn;
+}
 void prettyUnder(const BigNum& lhs, const BigNum& rhs, const BigNum& result, char op, size_t maxSize, size_t where, std::ostream& out = std::cout) {
     gotoxy(1, 1);
+
     out << "Calculating " << lhs.number << " " << op << " " << rhs.number << clr << std::endl;
 
     gotoxy(1, 3);
@@ -132,6 +160,8 @@ void printOp(char nd1, char nd2, char op, int rem, int result, std::ostream& out
 
 
 bool operator<(const BigNum& lhs, const BigNum& rhs) {
+    debug << lhs.number << " " << (lhs.outp == &std::cout) << " < " << rhs.number << " " << (rhs.outp == &std::cout) << std::endl;
+
     if(lhs.number.size() != rhs.number.size()) {
         return lhs.number.size() < rhs.number.size();
     }
@@ -149,6 +179,8 @@ bool operator<(const BigNum& lhs, const BigNum& rhs) {
 }
 
 BigNum operator+(const BigNum& lhs, const BigNum& rhs) {
+    debug << lhs.number << " " << (lhs.outp == &std::cout) << " + " << rhs.number << " " << (rhs.outp == &std::cout) << std::endl;
+
     BigNum result(*lhs.outp);
     std::ostream& out = *lhs.outp;
     std::size_t maxSize = std::max(lhs.number.size(), rhs.number.size()) + 1;
@@ -198,7 +230,7 @@ BigNum operator+(const BigNum& lhs, const BigNum& rhs) {
         }
 
         remainder = next > 9;
-        if(out == &std::cout) {
+        if(&out == &std::cout) {
             std::cin.get();
         }
     }
@@ -208,18 +240,20 @@ BigNum operator+(const BigNum& lhs, const BigNum& rhs) {
 
 
 BigNum operator-(const BigNum& lhsx, const BigNum& rhsx) {
+    debug << lhsx.number << " " << (lhsx.outp == &std::cout) << " - " << rhsx.number << " " << (rhsx.outp == &std::cout) << std::endl;
+
     bool swapped = lhsx < rhsx;
 
     const BigNum& lhs(swapped ? rhsx : lhsx);
     const BigNum& rhs(swapped ? lhsx : rhsx);
 
-    BigNum result;
+    BigNum result(*lhs.outp);
     std::ostream& out = *lhsx.outp;
 
     if(swapped) {
         gotoxy(1, 1);
         out << "The first argument is smaller. We switch the arguments, and negate the result." << std::endl;
-        if(out == &std::cout) {
+        if(&out == &std::cout) {
             std::cin.get();
         }
     }
@@ -279,10 +313,15 @@ BigNum operator-(const BigNum& lhsx, const BigNum& rhsx) {
         }
 
         remainder = next < 0;
-        if(out == &std::cout) {
+
+        if(&out == &std::cout) {
             std::cin.get();
         }
     }
+    while(result.number.size() > 1 && result.number.front() == '0') {
+        result.number = result.number.substr(1);
+    }
+
     if(swapped) {
         out << "We negate the result." << std::endl;
         result += '-';
@@ -293,12 +332,30 @@ BigNum operator-(const BigNum& lhsx, const BigNum& rhsx) {
 
 
 BigNum operator*(const BigNum& lhs, const BigNum& rhs) {
+    debug << lhs.number << " " << (lhs.outp == &std::cout) << " * " << rhs.number << " " << (rhs.outp == &std::cout) << std::endl;
+    if((lhs.number.size() == 1 && lhs.number == "0") ||
+        (rhs.number.size() == 1 && rhs.number == "0"))
+    {
+        std::ostream& out = *lhs.outp;
+        BigNum result(*lhs.outp);
+        result.number = "0";
+        gotoxy(1, 1);
+        out << "Calculating " << lhs.number << " * " << rhs.number << clr << std::endl;
+        gotoxy(1, 3);
+        out << "Zero multiply, the result is zero." << std::endl;
+        if(&out == &std::cout) {
+            std::cin.get();
+        }
+        return result;
+    }
+
     std::ostream& out = *lhs.outp;
     std::vector<BigNum> halfResults;
     int remainder = 0;
+    std::stringstream ss;
 
     for(auto i = 0UL; i < rhs.number.size(); ++i) {
-        halfResults.emplace_back();
+        halfResults.emplace_back(ss);
         halfResults.back().number = std::string(rhs.number.size() - i - 1, '0');
         remainder = 0;
         auto itl = lhs.number.rbegin();
@@ -322,17 +379,17 @@ BigNum operator*(const BigNum& lhs, const BigNum& rhs) {
 
             remainder = next / 10;
 
-            std::cin.get();
+            if(&out == &std::cout) {
+                std::cin.get();
+            }
         }
     }
 
-    std::stringstream ss;
-    BigNum result(ss);
+    BigNum result(*lhs.outp);
     result.number = "0";
     for(BigNum& half : halfResults) {
-        result = result + half;
+        result = half + result;
     }
-
     prettyMul(lhs, rhs, halfResults, '*', rhs.number.size() * lhs.number.size() - 1, out);
 
     gotoxy(1, 5 + rhs.number.size());
@@ -343,15 +400,131 @@ BigNum operator*(const BigNum& lhs, const BigNum& rhs) {
     out << std::string(max, '-') << clr << std::endl;
     gotoxy(1, 7 + rhs.number.size());
     result.print(max - result.number.size(),out) << clr << std::endl;
+    result.outp = lhs.outp;
 
     return result;
 }
 
-BigNum operator/(const BigNum& lhs, const BigNum& rhs) {
+
+void prettyDiv(const BigNum& lhs, const BigNum& rhs, const std::vector<BigNum>& remainders, const BigNum& result, std::size_t pg, std::ostream& out = std::cout) {
+    gotoxy(1, 1);
+    out << "Calculating " << lhs.number << " / " << rhs.number << clr << std::endl;
+
+    gotoxy(1, 3);
+    out << std::string(pg - 1, ' ') << "|" << clr;
+    gotoxy(1, 4);
+    out << lhs.number << ":" << rhs.number << "=" << result.number << clr << std::endl;
+    for(auto i = 0UL; i < remainders.size(); ++i) {
+        gotoxy(1, i+5);
+        auto& bnum = remainders.at(i);
+        bnum.print(i + 1 - bnum.number.size()) << clr;
+    }
+    gotoxy(1, 5 + remainders.size());
+    out << clr;
+    gotoxy(1, 6 + remainders.size());
+    out << clr;
+    gotoxy(1, 7 + remainders.size());
+    out << clr;
+    gotoxy(1, 6 + remainders.size());
+}
+
+std::pair<BigNum, BigNum> operator/(const BigNum& lhs, const BigNum& rhs) {
+    debug << lhs.number << " " << (lhs.outp == &std::cout) << " / " << rhs.number << " " << (rhs.outp == &std::cout) << std::endl;
     std::ostream& out = *lhs.outp;
 
+    BigNum result(*lhs.outp);
 
-    return lhs;
+
+    if(lhs < rhs) {
+        result.number = "0";
+
+        gotoxy(1, 1);
+        out << "Calculating " << lhs.number << " / " << rhs.number << clr << std::endl;
+        gotoxy(1, 3);
+        out << "The first argument is smaller than second." << std::endl;
+        out << "The result is 0, and the remainder is the first argument." << std::endl;
+
+        if(&out == &std::cout) {
+            std::cin.get();
+        }
+        return {result, lhs};
+    }
+    if(rhs.number[0] == '0') {
+        result.number = "ERR";
+        gotoxy(1, 1);
+        out << "Calculating " << lhs.number << " / " << rhs.number << clr << std::endl;
+        gotoxy(1, 3);
+        out << "Divisor is zero." << std::endl;
+
+        return {result, result};
+    }
+
+    std::vector<BigNum> remainders;
+    std::stringstream ss;
+    remainders.emplace_back(ss);
+
+    BigNum rhscp = rhs;
+    rhscp.outp = &ss;
+
+    int i = 0;
+    auto itl = lhs.number.begin();
+    while(itl != lhs.number.end()) {
+        if(remainders.back().number[0] == '0') {
+            remainders.back().number = *itl;
+        }
+        else {
+            remainders.back().number.push_back(*itl);
+        }
+
+        BigNum bn(ss), prevBn(ss);
+        bn.number = "0";
+        BigNum mul, prevMul;
+
+        mul = rhscp * bn;
+
+        while(!(remainders.back() < mul)) {
+            prevMul = mul;
+            prevBn = bn;
+
+            ++bn;
+            mul = rhscp * bn;
+        }
+
+        if(prevBn.number[0] != '0') {
+            if(result.number.size() == 0 || result.number[0] == '0') {
+                result.number = prevBn.number;
+            }
+            else {
+                result.number += prevBn.number;
+            }
+        }
+
+        prettyDiv(lhs, rhs, remainders, result, ++i, out);
+
+        out << remainders.back().number << " : " << rhs.number << " = " << prevBn.number;
+
+        if(&out == &std::cout) {
+            std::cin.get();
+        }
+
+        remainders.emplace_back(remainders.back() - prevMul);
+
+        remainders.back().number.push_back(' ');
+        prettyDiv(lhs, rhs, remainders, result, i, out);
+        remainders.back().number.pop_back();
+        out << prevBn.number << " * " << rhs.number << " = " << prevMul.number << std::endl;
+        out << (++remainders.rbegin())->number << " - " << prevMul.number << " = " << remainders.back().number << std::endl;
+
+        if(&out == &std::cout) {
+            std::cin.get();
+        }
+
+        ++itl;
+    }
+    remainders.back().number.push_back(' ');
+    prettyDiv(lhs, rhs, remainders, result, i, out);
+    remainders.back().number.pop_back();
+    return std::make_pair(result, remainders.back());
 }
 
 int main()
@@ -366,8 +539,8 @@ int main()
 
 
         gotoxy(1, 1);
-        for(int i = 0; i < 20; ++i)
-        std::cout << clr << "\n";
+        for(int i = 0; i < 70; ++i)
+            std::cout << clr << "\n";
 
         if(operatorIndex == std::string::npos) {
             assert(false);
@@ -376,7 +549,9 @@ int main()
         firstOperand = task;
 
         if(task[operatorIndex] == 'g') {
-            firstOperand.sqrt();
+            auto sqrt = firstOperand.sqrt();
+            std::cout << "sqrt("  << firstOperand.number << ") = " << sqrt.number << std::endl;
+            std::cout << std::endl << " next : ";
             continue;
         }
 
@@ -384,6 +559,7 @@ int main()
         secondOperand = task.substr(operatorIndex);
 
         BigNum result;
+        BigNum result2;
         switch(task[operatorIndex]) {
         case '+':
             result = firstOperand + secondOperand;
@@ -395,12 +571,16 @@ int main()
             result = firstOperand * secondOperand;
         break;
         case '/':
-            result = firstOperand / secondOperand;
+            std::tie(result, result2) = firstOperand / secondOperand;
         break;
         }
         std::cout << "Result is: ";
 
-        result.print() << std::endl << std::endl;
-        std::cout << " next : ";
+        result.print() << std::endl;
+        if(result2.number.size()) {
+            std::cout << "Remainder: ";
+            result2.print() << std::endl;
+        }
+        std::cout << std::endl << " next : ";
     }
 }
